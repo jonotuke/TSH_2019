47596556427bbfdb05c20774a52c2a645a02e768
Jono Tuke <jonotuke@gmail.com>
2019-05-30 19:06:08 +0930

    Primary analysis

---
+++ code/get_cov.R
@@ -0,0 +1,31 @@
+pacman::p_load(glue)
+get_cov  <- function(cov){
+  f1  <- as.formula(glue("Sig ~ Thyroid_test * {cov} + (1 | ID)"))
+  M1  <- glmer(f1, data = TSH_long, family = binomial)
+  f2  <- as.formula(glue("Sig ~ Thyroid_test + {cov} + (1 | ID)"))
+  M2  <- glmer(f2, data = TSH_long, family = binomial)
+  f3  <- as.formula(glue("Sig ~ Thyroid_test + (1 | ID)"))
+  M3  <- glmer(f3, data = TSH_long, family = binomial)
+  f4  <- as.formula(glue("Sig ~ Thyroid_test * {cov}"))
+  M4  <- glm(f4, data = TSH_long, family = binomial)
+  f5  <- as.formula(glue("Sig ~ Thyroid_test + {cov}"))
+  M5  <- glm(f5, data = TSH_long, family = binomial)
+  f6  <- as.formula(glue("Sig ~ Thyroid_test"))
+  M6  <- glm(f6, data = TSH_long, family = binomial)
+  models  <- list(M1, M2, M3, M4, M5, M6)
+  model_info  <- models %>% map_df(broom::glance)
+  get_converge  <- function(M){
+    if("glm" %in% class(M)){
+      return(M$converged)
+    }
+    code  <- M@optinfo$conv$lme4$code
+    if(is.null(code)){
+      return(TRUE)
+    }
+    return(FALSE)
+  }
+  model_info$converge  <- models %>% map_lgl(get_converge)
+  model_info$models  <- c("interaction + RI", "test + main + RI", "test + RI", 
+                          "interaction", "test + main", "test")
+  return(model_info)
+}
---
+++ code/visualise_ci.R
@@ -0,0 +1,51 @@
+#' Give asymptotic CI for proportion of significant
+#'
+#' @param df data frame with data needs column called Thyroid method and Sig
+#' @param pred predictor
+#' @param ci confidence level
+#' @param width errorbars width
+#' @param dodge how much to separate bars
+#' @param check can return table instead of plot
+#'
+#' @return
+#' @export
+#'
+#' @examples
+visualise_ci  <- function(df, pred, ci = 0.95, width = 0.1, dodge = 0.4, check = FALSE){
+  pd <- position_dodge(width = dodge)
+  ## Enquo the column
+  pred  <- enquo(pred)
+  ## Get rid of missing values in sig
+  df  <- df %>% 
+    filter(!is.na(Sig))
+  ## Get rid of missing values in pred
+  df  <- df %>% 
+    filter(!is.na(!!pred))
+  # Get cutoff
+  ci  <- (1 - ci) / 2
+  z_star  <- qnorm(ci, lower.tail = FALSE)
+  # Calculate CI
+  df_ci  <- 
+    df %>% 
+    group_by(Thyroid_test, !!pred) %>% 
+    summarise(
+      n_sig = sum(Sig == "Y"),
+      N = n(), 
+      p = n_sig / N, 
+      # convert - lower to zero
+      lwr = max(0, p - 1.96 * sqrt(p * (1 - p) / N)),
+      # bound upr to one
+      upr = min(1, p + 1.96 * sqrt(p * (1 - p) / N))
+    )
+  if(check){
+    return(df_ci)
+  }
+  ## Plot
+  p  <- df_ci %>% 
+    ggplot(aes(!!pred, p, col = Thyroid_test)) + 
+    geom_point(position = pd) + 
+    geom_errorbar(aes(ymin = lwr, ymax = upr), width = width, position = pd) + 
+    theme(axis.text.x = element_text(angle = -90, hjust=0)) + 
+    labs(y = "Proportion")
+  return(p)
+}
